472043206e3eb5297692821d2cb1d19844e7cb72
Rémi Jarasson <remi.jarasson@gmail.com>
2022-10-07 22:48:07 +0200

    feat: added swap gradient color stops

--- src/components/PaletteForm/PaletteFormLinear.js
+++ src/components/PaletteForm/PaletteFormLinear.js
@@ -13,9 +13,14 @@ function PaletteFormLinear({ onChange, value = {} }) {
     onChange({ ...value, angle: Number(event.target.value) });
   };
 
+  const handleSwap = () => [onChange({ ...value, start: value.stop, stop: value.start })];
+
   return (
     <div className="PaletteForm-linear">
       <input className="PaletteForm-picker" type="color" value={value.start} onChange={handleChangeStart} />
+      <button type="button" className="PaletteForm-swap" onClick={handleSwap}>
+        ⇄
+      </button>
       <input className="PaletteForm-picker" type="color" value={value.stop} onChange={handleChangeStop} />
       <div className="PaletteForm-slider">
         <div className="PaletteForm-sliderLabel">{`${value.angle}°`}</div>
--- src/components/PaletteForm/PaletteFormRadial.js
+++ src/components/PaletteForm/PaletteFormRadial.js
@@ -13,9 +13,14 @@ function PaletteFormRadial({ onChange, value = {} }) {
     onChange({ ...value, offset: Number(event.target.value) });
   };
 
+  const handleSwap = () => [onChange({ ...value, start: value.stop, stop: value.start })];
+
   return (
     <div className="PaletteForm-radial">
       <input className="PaletteForm-picker" type="color" value={value.start} onChange={handleChangeStart} />
+      <button type="button" className="PaletteForm-swap" onClick={handleSwap}>
+        ⇄
+      </button>
       <input className="PaletteForm-picker" type="color" value={value.stop} onChange={handleChangeStop} />
       <div className="PaletteForm-slider">
         <div className="PaletteForm-sliderLabel">{`${value.offset}%`}</div>
